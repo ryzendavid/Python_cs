d5da54e2a65229cde168abc38d974c4a918e2aab
RaizenDavid <davido9634@gmail.com>
2025-09-30 00:19:16 +0100

    - Data structures and algorithms

---
+++ #Data structures and algorithms.py
@@ -0,0 +1,16 @@
+#Data structures and algorithms.... 
+
+#Arrays.. 
+
+#Arrays are good for reading as data is stored in indexes.. 0(1)
+
+#Linked List are just like arrays but elements can be stored anywhere in memory. Each Node(data) points to the next.. 0(n) this makes it easy for inserting and deleting but terrible for reading.. 
+
+#use arrays for fast access when the size is known and linked list for fast insertion/deletion...
+
+
+
+
+# ---- Hasmaps.. 
+
+#Hashmaps are also like arrays but we can choose what index is(key) and data and key are stored as kvp. Hasp map is basically dictionary in python and other programming langiages... Hashmap is also unordered is has fast searching... 
